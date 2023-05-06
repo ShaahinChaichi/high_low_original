0185ed93bbcf5958b48fd669aa252c5b1148f2e6
ShaahinChaichi <shaahin1974@gmail.com>
2023-05-06 17:38:19 +0330

    Function "check_answer" add

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+from art import logo, vs
 from game_data import data
 import random
 
+# print(logo)
+
 
 def format_data(account):
-    """Format the data into printable format"""
+    """Takes the account data and returns the printable format"""
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
     return f"{account_name}, a {account_descr} from {account_country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """ Check the user if correct"""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
 
 # Generate a random account from data
@@ -16,5 +27,20 @@ account_b = random.choice(data)
 if account_a == account_b:
     account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)} with {format_data(account_b)}")
+print(f"Compare A: {format_data(account_a)}")
+# print(vs)
+print(f"Against B: {format_data(account_b)}")
 
+# Ask the user to guess
+guess = input("Who has more followers A or B ? ").lower()
+
+a_follower_count = account_a['follower_count']
+b_follower_count = account_b['follower_count']
+
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+# Give feedback to user:
+if is_correct:
+    print("you're right..")
+else:
+    print("Sorry, you're wrong")
