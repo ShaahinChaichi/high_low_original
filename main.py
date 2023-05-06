635ca5cc1d36e27f02d71bfa232f6ad474270b47
ShaahinChaichi <shaahin1974@gmail.com>
2023-05-06 16:49:11 +0330

    Function "Format data" add

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from game_data import data
 import random
 
 
+def format_data(account):
+    """Format the data into printable format"""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr} from {account_country}"
+
+
+# Generate a random account from data
 account_a = random.choice(data)
 account_b = random.choice(data)
+if account_a == account_b:
+    account_b = random.choice(data)
 
-print(account_a['name'])
+print(f"Compare A: {format_data(account_a)} with {format_data(account_b)}")
+
